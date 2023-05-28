283b9336cb13b2df138621aed7e8f03e52e588e1
Dima <d.ovcharenko90@gmail.com>
2023-05-28 19:22:35 +0300

    another fix

--- src/main/java/com/clouds42/Commands/SendMessageCommand.java
+++ src/main/java/com/clouds42/Commands/SendMessageCommand.java
@@ -82,7 +82,7 @@ public class SendMessageCommand implements Callable<Integer> {
                 Thread.sleep(10);
             }
         }
-        if (result.equals(PipeMessages.OK_RESULT)) {
+        if (result != null && result.equals(PipeMessages.OK_RESULT)) {
             logger.info("Command success: " + commandText);
             client.close();
             return CommandLine.ExitCode.OK;
